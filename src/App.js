74e7283de28f2e4c65dd8703c059d7885e838872
James Seidel <james.seidel@2128-072021.localdomain>
2024-06-28 15:06:29 +0200

    fix headline for citycard

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
 import { useEffect, useState } from "react";
 import "./App.css";
- import CityCard from "./CityCard";
- import cloudySkies from "./images/cloudy.svg";
+import CityCard from "./CityCard";
+import cloudySkies from "./images/cloudy.svg";
 
 function App() {
   const [cityData, setCityData] = useState({});
-  const cities = ["Hamburg"];
+  const cities = ["Hamburg", "New+York", "Paris"];
 
   const fetchData = async (city) => {
     const response = await fetch(
-      `http://api.openweathermap.org/data/2.5/weather?q=${city}&APPID=c6f9c5276d42f0a32ce26ae3d1334051`
+      `http://api.openweathermap.org/data/2.5/weather?q=${city}&APPID=c6f9c5276d42f0a32ce26ae3d1334051&units=metric`
     );
     const data = await response.json();
     return data;
@@ -33,15 +33,14 @@ function App() {
     if (!cityData) return null;
     const data = cityData[city];
     const isLoading = !data;
-
     return (
       <div key={city}>
-        <h1>{city}</h1>
+        <h1>{data.name}</h1>
         {isLoading ? (
           <p>Loading...</p>
         ) : (
           <CityCard
-            name={data.sys.name}
+            name={data.name}
             temp={data.main.temp}
             imageSrc={cloudySkies}
           />
