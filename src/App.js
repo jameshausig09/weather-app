6e4471fd3cf14428ea920c43075c0b42e342ee1f
James Seidel <james.seidel@2128-072021.localdomain>
2024-06-28 14:25:27 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,12 +1,47 @@
+import { useEffect, useState } from "react";
 import "./App.css";
+import CityCard from "./CityCard";
+import cloudySkies from "./images/cloudy.svg";
+import axios from "axios";
 
 function App() {
+  const [weatherData, setWeatherData] = useState({});
+  async function fetchData(city) {
+    let url = `
+https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${city}?unitGroup=us&key=TKSNR9R5STMBCQ8HM2Z9GPZPB&contentType=json`;
+    const RESPONSE = await axios.get(url);
+    setWeatherData(RESPONSE.data);
+  }
+  const cities = ["hamburg"];
+
+  function renderCityCard() {
+    return cities.map(async (city) => {
+      const DATA = await fetchData(city);
+      console.log(DATA);
+      return (
+        <CityCard
+          name={DATA.resolvedAddress}
+          temp={DATA.currentConditions.temp}
+          imageSrc={cloudySkies}
+        />
+      );
+    });
+  }
+
   return (
     <div className="App">
       <header className="App-header">
-        <h1>Weather</h1>
-        <h2>Hamburg</h2>
+        {/* citycard */}
+        {renderCityCard()}
       </header>
+      <nav className="Navigation">
+        <ul>
+          <li>Home</li>
+          <li>Search</li>
+          <li>Calendar</li>
+          <li>Custom</li>
+        </ul>
+      </nav>
     </div>
   );
 }
