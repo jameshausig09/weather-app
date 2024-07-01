af9aa824270375da0e9e2f6b3d94756153afc718
James Seidel <james.seidel@2128-072021.localdomain>
2024-07-01 14:48:32 +0200

    working on the search tab

--- src/pages/Search.js
+++ src/pages/Search.js
@@ -1,6 +1,18 @@
+import CityCard from "../CityCard";
+import cloudySkies from "../images/cloudy.svg";
+import "./Search.css";
+
 const Search = () => {
   // hey James! So have a look at your wireframe in Excalidraw, and start to make the hardcoded layout of your search
   // page, and then we can start to make it dynamic by adding the search functionality.
-  return <h1>Search</h1>;
+  const cities = ["Hamburg", "New+York", "Paris"];
+  return (
+    <div className="Search">
+      <input type="text" placeholder="Search.."></input>
+      {cities.map((city) => (
+        <CityCard name={city} temp={0} imageSrc={cloudySkies} />
+      ))}
+    </div>
+  );
 };
 export default Search;
