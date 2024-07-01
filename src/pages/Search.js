a5ddbb018424a9a5a74508fd369ccee1954308ad
hugh.burgess@db-n.com <hugh.burgess@db-n.com>
2024-07-01 16:01:38 +0200

    search page comment - step 3

--- src/pages/Search.js
+++ src/pages/Search.js
@@ -14,14 +14,9 @@ const Search = () => {
             city.toLowerCase().includes(ev.target.value)
           );
           console.log("filteredCities", filteredCities);
+          // this is almost there! How about you move this logic up to a function above the return line, and then use the new array in line 20 instead of "cities"?
         }}
       ></input>
-      {/* // ok this is great, now imagine that when you type in the search, you want the array to show the cities that match the search as you type them, 
-      // so if you type "H" you should see "Hamburg" and if you type "Ha" you should see "Hamburg" and so on,
-      // how would you do that?
-
-      // I would use the filter method to filter the cities array based on the input value. 
-// Have a look at how to use a filter function: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/filter */}
       {cities.map((city) => (
         <CityCard name={city} temp={0} imageSrc={cloudySkies} />
       ))}
