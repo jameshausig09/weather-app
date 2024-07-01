a5953d161455043a347e628863b4202d98d48c95
hugh.burgess@db-n.com <hugh.burgess@db-n.com>
2024-07-01 14:52:15 +0200

    search page comment - next step

--- src/pages/Search.js
+++ src/pages/Search.js
@@ -3,12 +3,16 @@ import cloudySkies from "../images/cloudy.svg";
 import "./Search.css";
 
 const Search = () => {
-  // hey James! So have a look at your wireframe in Excalidraw, and start to make the hardcoded layout of your search
-  // page, and then we can start to make it dynamic by adding the search functionality.
   const cities = ["Hamburg", "New+York", "Paris"];
   return (
     <div className="Search">
       <input type="text" placeholder="Search.."></input>
+      // ok this is great, now imagine that when you type in the search, you want the array to show the cities that match the search as you type them, 
+      // so if you type "H" you should see "Hamburg" and if you type "Ha" you should see "Hamburg" and so on,
+      // how would you do that?
+
+      // I would use the filter method to filter the cities array based on the input value. 
+// Have a look at how to use a filter function: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/filter
       {cities.map((city) => (
         <CityCard name={city} temp={0} imageSrc={cloudySkies} />
       ))}
