4afff69adabef1c9cb47b4b4a4b6cd2359e30fea
hugh.burgess@db-n.com <hugh.burgess@db-n.com>
2024-07-01 12:04:07 +0200

    search page comment

--- src/pages/Search.js
+++ src/pages/Search.js
@@ -1,5 +1,6 @@
 const Search = () => {
+  // hey James! So have a look at your wireframe in Excalidraw, and start to make the hardcoded layout of your search
+  // page, and then we can start to make it dynamic by adding the search functionality.
   return <h1>Search</h1>;
 };
-
 export default Search;
